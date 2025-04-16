1533b9600d5115476608f07aabddd3d1b2dd0048
anmol1322 <assg.cool@gmail.com>
2025-04-16 10:15:26 +0000

    Created Services and controller 1604:3.45pm

--- dotnetapp/Controllers/PlantController.cs
+++ dotnetapp/Controllers/PlantController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+using dotnetapp.Services;
 
 namespace dotnetapp.Controllers
 {
@@ -10,6 +13,69 @@ namespace dotnetapp.Controllers
     [Route("api/[controller]")]
     public class PlantController : ControllerBase
     {
+        private readonly PlantService _plantService;
+        public PlantController(PlantService plantService){
+            _plantService = plantService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plant>>>GetAllPlants(){
+            var plants =await _plantService.GetAllPlants();
+            return Ok(plants);
+        }
         
+        [HttpGet("{plantId}")]
+        public async Task <ActionResult<Plant>>GetPlantById(int plantId){
+            var plant=await _plantService.GetPlantById(plantId);
+            if(plant==null){
+                return NotFound("Cannot find any plant");
+            }
+            return Ok(plant);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>AddPlant([FromBody] Plant plant){
+            try{
+                var result=await _plantService.AddPlant(plant);
+                if(result){
+                    return Ok("Plant added successfully");
+                }
+                return StatusCode(500,"Failed to add plant");
+            }
+            catch(Exception ex){
+                return StatusCode(500,ex.Message);
+            }
+        }
+
+        [HttpPut("{plantId}")]
+        public async Task<ActionResult>UpdatePlant(int plantId, [FromBody] Plant plant){
+            try{
+                var result=await _plantService.UpdatePlant(plantId,plant);
+                if(result){
+                    return Ok("Plant updated successfully");
+                }
+                return NotFound("Cannot find any plant");
+            }
+            catch(Exception ex){
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{plantId}")]
+        public async Task<ActionResult> DeletePlant(int plantId)
+        { try
+            {
+                var result = await _plantService.DeletePlant(plantId);
+                if (result)
+                {
+                    return Ok("Plant deleted successfully");
+                }
+                return NotFound("Cannot find any plant");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+using dotnetapp.Services;
+using dotnetapp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("constr")));
 
+builder.Services.AddScoped<PlantService>();
 builder.Services.AddControllers();
 
 
@@ -9,7 +21,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 
 var app = builder.Build();
