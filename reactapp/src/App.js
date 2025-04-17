29eac6a3eebd944640fcc593b7f1bdff43ffa738
AkshatJain1900 <jainakshat1900@gmail.com>
2025-04-17 05:54:24 +0000

    updatedfilesbyanmol

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
+import HomePage from './Components/HomePage'
 const App = () => {
   return (
     <div>
         <Signup />
-       <Login />
-        <Homepage />
+        <Login />
+        <HomePage />
         
     </div>
   )
