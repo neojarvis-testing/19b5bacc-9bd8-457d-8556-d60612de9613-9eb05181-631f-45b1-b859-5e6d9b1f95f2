ce24c277e054b5a66a75ba2dda32adbf06e13a1b
sakthii3 <sakthivels1k2@gmail.com>
2025-04-17 12:10:26 +0000

    Added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import Login from './Components/Login'
-import Signup from './Components/Signup'
 import ErrorPage from './Components/ErrorPage'
-import HomePage from './Components.HomePage'
+import Signup from './Components/Signup'
+import HomePage from './Components/HomePage'
+import GardenerNavbar from './GardenerComponents/GardenerNavbar'
+import CustomerNavbar from './CustomerComponents/CustomerNavbar'
+import { BrowserRouter } from 'react-router-dom';
+import ViewPlant from './GardenerComponents/ViewPlant'
+import PlantForm from './GardenerComponents/PlantForm'
 
 const App = () => {
   return (
     
 <BrowserRouter>
-
     <div>
         <CustomerNavbar/>
         <Signup />
         <Login />
         <GardenerNavbar/>
         <HomePage />
+        <ViewPlant/>
+        <PlantForm/>
         <Login></Login>
         <ErrorPage></ErrorPage>
-        
     </div>
-    
 </BrowserRouter>
 
   )
