b3dcb79ad19cd17cf680d0cade8fab579da865bd
anmol1322 <assg.cool@gmail.com>
2025-04-17 12:08:46 +0000

    files

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import Login from './Components/Login'
-<<<<<<< HEAD
-import ErrorPage from './Components/ErrorPage'
-=======
 import Signup from './Components/Signup'
 import HomePage from './Components/HomePage'
->>>>>>> 1073315c01b44b0953b6d2982aaed64ef873af7a
 const App = () => {
   return (
     <div>
@@ -13,11 +9,7 @@ const App = () => {
         <Login />
         <HomePage />
         <Login></Login>
-<<<<<<< HEAD
-        <ErrorPage></ErrorPage>
-=======
          
->>>>>>> 1073315c01b44b0953b6d2982aaed64ef873af7a
         
     </div>
   )
